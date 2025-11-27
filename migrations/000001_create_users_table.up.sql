23551e4e72d308db3460a251a7714268fbfd8826
voloshmiak <voloshenkomykhailo@gmail.com>
2025-11-27 12:14:10 +0200

    Update users table to remove uniqueness constraint on first and last name

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users
 (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) UNIQUE NOT NULL,
-    last_name VARCHAR(50) UNIQUE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
